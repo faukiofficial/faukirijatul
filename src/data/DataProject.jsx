2034233adfe1a19a9865fff3952365738521afc2
faukiofficial <faukiofficial@gmail.com>
2024-07-19 23:52:39 +0700

    add JagoCatat

--- src/data/DataProject.jsx
+++ src/data/DataProject.jsx
@@ -1,13 +1,16 @@
 import {
   FaCss3Alt,
   FaReact,
+  FaNode,
 } from "react-icons/fa";
 import { DiJavascript } from "react-icons/di";
 import { RiTailwindCssFill } from "react-icons/ri";
 import { FaHtml5 } from "react-icons/fa";
+import { TbBrandVite } from "react-icons/tb";
+import { SiExpress, SiMysql, SiMongodb } from "react-icons/si";
 
 import project1Image from "../assets/faukirijatul.svg";
-import project2Image from "../assets/Project2.svg";
+import project2Image from "../assets/jagocatat.svg";
 import project3Image from "../assets/Project3.svg";
 import project4Image from "../assets/Project4.svg";
 import project5Image from "../assets/Project5.svg";
@@ -20,9 +23,26 @@ import project11Image from "../assets/Project11.svg";
 import project12Image from "../assets/Project12.svg";
 
 const projectList = [
-  {
+    {
     id: "1",
-    title: "Fauki Rijatul Personal Web",
+    title: "NoteApp: JagoCatat",
+    image: project2Image,
+    linkDemo: "https://jagocatat.onrender.com/dashboard",
+    linkGithub: "https://github.com/faukiofficial/JagoCatat.git",
+    tool: [
+      <FaHtml5 key="html" className="inline-block text-[#E34F26] w-12 h-auto m-2" title="HTML" />,
+      <DiJavascript key="javascript" className="inline-block text-[#F7DF1E] w-12 h-auto m-2" title="JavaScript"/>,
+      <RiTailwindCssFill key="tailwindcss" className="inline-block text-[#38B2AC] w-12 h-auto m-3" title="Tailwind CSS"/>,
+      <TbBrandVite key="vite" className="inline-block text-[#646cff] w-12 h-auto m-3" title="Vite"/>,
+      <FaReact key="reactjs" className="inline-block text-[#61DAFB] w-12 h-auto m-3" title="ReactJS"/>,
+      <FaNode key="nodejs" className="inline-block text-[#339933] w-12 h-auto m-3" title="NodeJS"/>,
+      <SiExpress key="expressjs" className="inline-block text-[#000000] w-12 h-auto m-3" title="ExpressJS"/>,
+      <SiMongodb key="mongodb" className="inline-block text-[#47A248] w-12 h-auto m-3" title="MongoDB"/>,
+    ],
+  },
+  {
+    id: "2",
+    title: "Personal Web: Fauki Rijatul",
     image: project1Image,
     linkDemo: "https://faukirijatul.vercel.app/",
     linkGithub: "https://github.com/faukiofficial/faukirijatul.git",
@@ -31,19 +51,8 @@ const projectList = [
       <FaCss3Alt key="css" className="inline-block text-[#1572B6] w-12 h-auto m-2" title="CSS" />,
       <DiJavascript key="javascript" className="inline-block text-[#F7DF1E] w-12 h-auto m-2" title="JavaScript"/>,
       <RiTailwindCssFill key="tailwindcss" className="inline-block text-[#38B2AC] w-12 h-auto m-3" title="Tailwind CSS"/>,
-      <FaReact key="reactjs" className="inline-block text-[#61DAFB] w-12 h-auto m-3" title="ReactJS"/>
-    ],
-  },
-  {
-    id: "2",
-    title: "New Project Will Be Added 2",
-    image: project2Image,
-    linkDemo: "https://web.facebook.com/faukiofficial?_rdc=1&_rdr",
-    linkGithub: "https://web.facebook.com/faukiofficial?_rdc=1&_rdr",
-    tool: [
-      <FaHtml5 key="html" className="inline-block text-[#E34F26] w-12 h-auto m-2" />,
-      <FaCss3Alt key="css" className="inline-block text-[#1572B6] w-12 h-auto m-2" />,
-      <DiJavascript key="javascript" className="inline-block text-[#F7DF1E] w-12 h-auto m-2" />,
+      <TbBrandVite key="tailwindcss" className="inline-block text-[#646cff] w-12 h-auto m-3" title="Vite"/>,
+      <FaReact key="reactjs" className="inline-block text-[#61DAFB] w-12 h-auto m-3" title="ReactJS"/>,
     ],
   },
   {
