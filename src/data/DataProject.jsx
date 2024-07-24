378600459ecbb5fe2c62a7553372bd881b59771c
faukiofficial <faukiofficial@gmail.com>
2024-07-24 14:42:38 +0700

    add CRUD App Project

--- src/data/DataProject.jsx
+++ src/data/DataProject.jsx
@@ -8,10 +8,11 @@ import { RiTailwindCssFill } from "react-icons/ri";
 import { FaHtml5 } from "react-icons/fa";
 import { TbBrandVite } from "react-icons/tb";
 import { SiExpress, SiMysql, SiMongodb } from "react-icons/si";
+import { FaSass } from "react-icons/fa6";
 
 import project1Image from "../assets/faukirijatul.svg";
 import project2Image from "../assets/jagocatat.svg";
-import project3Image from "../assets/Project3.svg";
+import project3Image from "../assets/simple_cruds_app.svg";
 import project4Image from "../assets/Project4.svg";
 import project5Image from "../assets/Project5.svg";
 import project6Image from "../assets/Project6.svg";
@@ -51,20 +52,25 @@ const projectList = [
       <FaCss3Alt key="css" className="inline-block text-[#1572B6] w-12 h-auto m-2" title="CSS" />,
       <DiJavascript key="javascript" className="inline-block text-[#F7DF1E] w-12 h-auto m-2" title="JavaScript"/>,
       <RiTailwindCssFill key="tailwindcss" className="inline-block text-[#38B2AC] w-12 h-auto m-3" title="Tailwind CSS"/>,
-      <TbBrandVite key="tailwindcss" className="inline-block text-[#646cff] w-12 h-auto m-3" title="Vite"/>,
+      <TbBrandVite key="vite" className="inline-block text-[#646cff] w-12 h-auto m-3" title="Vite"/>,
       <FaReact key="reactjs" className="inline-block text-[#61DAFB] w-12 h-auto m-3" title="ReactJS"/>,
     ],
   },
   {
     id: "3",
-    title: "New Project Will Be Added 3",
+    title: "Simple CRUDS App",
     image: project3Image,
-    linkDemo: "https://web.facebook.com/faukiofficial?_rdc=1&_rdr",
-    linkGithub: "https://web.facebook.com/faukiofficial?_rdc=1&_rdr",
+    linkDemo: "https://github.com/faukiofficial/tugas-api-express-mongo.git",
+    linkGithub: "https://tugas-api-express-mongo.onrender.com/",
     tool: [
-      <FaHtml5 key="html" className="inline-block text-[#E34F26] w-12 h-auto m-2" />,
-      <FaCss3Alt key="css" className="inline-block text-[#1572B6] w-12 h-auto m-2" />,
-      <DiJavascript key="javascript" className="inline-block text-[#F7DF1E] w-12 h-auto m-2" />,
+      <FaHtml5 key="html" className="inline-block text-[#E34F26] w-12 h-auto m-2" title="HTML" />,
+      <DiJavascript key="javascript" className="inline-block text-[#F7DF1E] w-12 h-auto m-2" title="JavaScript"/>,
+      <FaSass key="sass" className="inline-block text-[#CF649A] w-12 h-auto m-2" title="sass" />,
+      <TbBrandVite key="vite" className="inline-block text-[#646cff] w-12 h-auto m-3" title="Vite"/>,
+      <FaReact key="reactjs" className="inline-block text-[#61DAFB] w-12 h-auto m-3" title="ReactJS"/>,
+      <FaNode key="nodejs" className="inline-block text-[#339933] w-12 h-auto m-3" title="NodeJS"/>,
+      <SiExpress key="expressjs" className="inline-block text-[#000000] w-12 h-auto m-3" title="ExpressJS"/>,
+      <SiMongodb key="mongodb" className="inline-block text-[#47A248] w-12 h-auto m-3" title="MongoDB"/>,
     ],
   },
   {
