148c3af6758d2b6eae544f9f4e9cc17700ef8f05
faukiofficial <faukiofficial@gmail.com>
2025-01-25 20:10:22 +0700

    ada projects total

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -8,7 +8,7 @@ const Project = () => {
   return (
     <section id="project" className="py-8 bg-gray-300">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 mt-10">
-        <h3 className="text-4xl font-semibold text-center text-gray-900 mb-8 uppercase">Projects</h3>
+        <h3 className="text-4xl font-semibold text-center text-gray-900 mb-8 uppercase">Projects ({projects.length})</h3>
         
           <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
           {projects.slice(0, 6).map((item, index) => (
