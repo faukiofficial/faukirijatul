b52d500456b16b9f4527596ab40b94a03dc22509
faukirijatul <faukirijatul42@gmail.com>
2025-03-18 09:19:38 +0700

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,11 @@ const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white text-center h-16 flex items-center justify-center">
       <span>&copy; {new Date().getFullYear()} -</span>
-      <Link className="hover:underline ml-1" to="/">
+      <Link
+        className="hover:underline ml-1"
+        to="/"
+        onClick={() => window.scrollTo({ top: 0, behavior: "smooth" })}
+      >
         Fauki Rijatul H
       </Link>
       <span>. All rights reserved</span>
