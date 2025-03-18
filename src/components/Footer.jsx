ca9ff625c53f93d9c38bdc57b384dfe805e221e1
faukirijatul <faukirijatul42@gmail.com>
2025-03-18 09:17:37 +0700

    Update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,9 +3,11 @@ import { Link } from "react-router-dom";
 const Footer = () => {
   return (
     <footer className="bg-gray-800 text-white text-center h-16 flex items-center justify-center">
-      <Link className="hover:underline" to="/">
-        &copy; Fauki Rijatul H{" "}
+      <span>&copy; {new Date().getFullYear()} -</span>
+      <Link className="hover:underline ml-1" to="/">
+        Fauki Rijatul H
       </Link>
+      <span>. All rights reserved</span>
     </footer>
   );
 };
